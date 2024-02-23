ccb415775c070dd220f760cce5eff7509c01f651
FERNANDO DE OLIVEIRA GUEDES <fernando.oguedes@senacsp.edu.br>
2024-02-22 21:41:16 -0300

    subindo projeto aula backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState(false)
+  function changeColor() {
+    setBackgroundColor(!backgroundColor)      
+  }
+  const [toggle, setToggle] = useState(false)
+  function changeDisplaySize() {
+    setToggle(!toggle)      
+  }  
+
+  const [fontSize, setFontSize] = useState('18px')
+  function changeFontSize(e){
+    console.log(e.target.id)
+    if (e.target.id ==='btnSmall'){
+      setFontSize('8px')
+    }
+    if (e.target.id ==='btnmedia'){
+      setFontSize('24px')
+    }
+    if (e.target.id === 'btnbig'){
+      setFontSize('38px')
+    }    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <div className='container'>
+                <header className='headerArea'>
+                  <button className='btn' onClick={changeColor}>Alterar cor</button>
+                  <button id='btnSmall' className='btn' onClick={changeFontSize}>Fonte Pequena</button>
+                  <button id='btnmedia' className='btn'onClick={changeFontSize}>Fonte Media</button>
+                  <button id='btnbig' className='btn'onClick={changeFontSize}>Fonte Grande</button>
+                  <button className='btn' onClick={changeDisplaySize}>Altera Tamanho</button>                   
+                </header>
+                <section className='sectionToggle'style={{display: toggle && 'flex'}}>
+                    <p>Area de exemplo para espaço que alterna</p>
+                </section>
+                <div className='content' style={{backgroundColor: backgroundColor && 'black'}}>
+                  <h2 style={{fontSize: fontSize}} >ALTERANDO O TAMANHO DAS FONTES</h2>
+                </div>
+              </div>
     </div>
   );
 }
